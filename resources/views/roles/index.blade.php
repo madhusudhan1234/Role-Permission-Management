cb11afa6a688dd11f7cb78eafbed4b173ace9956
madhusudhan1234 <madhu@acd.edu.np>
2016-09-07 14:09:32 +0545

    Solving the can middleware issues

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -7,7 +7,7 @@
                 <h2>Role Management</h2>
             </div>
             <div class="pull-right">
-                @permission(['role-create'])
+                @permission(('role-create'))
                     <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
                 @endpermission
             </div>
@@ -32,10 +32,10 @@
                 <td>{{ $role->description }}</td>
                 <td>
                     <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-                    @permission(['role-edit'])
+                    @permission(('role-edit'))
                         <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
                     @endpermission
-                    @permission(['role-delete'])
+                    @permission(('role-delete'))
                         {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                         {!! Form::close() !!}
